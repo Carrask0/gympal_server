372af4b2c948d0e464b911293a4d5368f236b6c2
Jorge Carrasco <jorge.carrasco@solucionapp.es>
2024-05-08 17:39:45 +0200

    v2

--- routes/gympal.js
+++ routes/gympal.js
@@ -1,77 +1,41 @@
 const express = require('express');
 const Service = require('../services/service');
+const { ObjectId } = require('mongodb');
 
 function gympalAPI(app) {
     const router = express.Router();
     app.use('/api', router);
     const service = new Service();
 
-    // EXERCISES
+    // GET ALL EXERCISES
     app.get('/exercises', async (req, res) => {
         try {
             const exercises = await service.getAll('exercises');
             if (exercises.length === 0) {
-                console.log("No exercises found");
                 res.status(404).json({ error: 'Exercises not found' }).sucess(true);
             } else {
-                console.log("Exercises found");
                 res.status(200).json({success: true, exercises});
             }
         } catch (error) {
-            console.error(error);
             res.status(500).json({ error: 'Error retrieving exercises' });
         }
     });
 
+    // GET EXERCISE BY ID
     app.get('/exercises/:id', async (req, res) => {
         try {
-            const exercise = await service.get('exercises', req.params.id);
+            const exercise = await service.get('exercises', { _id: new ObjectId(req.params.id) });
             if (exercise === null) {
-                console.log("Exercise not found");
                 res.status(404).json({ error: 'Exercise not found' });
             } else {
-                console.log("Exercise found");
                 res.status(200).json({success: true, exercise});
             }
         } catch (error) {
-            console.error(error);
             res.status(500).json({ error: 'Error retrieving exercise' });
         }
     });
-
-
-    // EXERCISE_STATS
-    app.get('/exercise_stats', async (req, res) => {
-        try {
-            const exerciseStats = await service.getAll('exercise_stats');
-            if (exerciseStats.length === 0) {
-                console.log("No exercise stats found");
-                res.status(404).json({ error: 'Exercise stats not found' });
-            } else {
-                console.log("Exercise stats found");
-                res.status(200).json({success: true, exerciseStats});
-            }
-        } catch (error) {
-            console.error(error);
-            res.status(500).json({ error: 'Error retrieving exercise stats' });
-        }
-    });
-
-    app.get('/exercise_stats/:id', async (req, res) => {
-        try {
-            const exerciseStat = await service.get('exercise_stats', req.params.id);
-            if (exerciseStat === null) {
-                res.status(404).json({ error: 'Exercise stat not found' });
-            } else {
-                res.status(200).json({success: true, exerciseStat});
-            }
-        } catch (error) {
-            console.error(error);
-            res.status(500).json({ error: 'Error retrieving exercise stat' });
-        }
-    });
  
-    // SESSIONS
+    // GET ALL SESSIONS
     app.get('/sessions', async (req, res) => {
         try {
             const sessions = await service.getAll('sessions');
@@ -86,9 +50,11 @@ function gympalAPI(app) {
         }
     });
 
+    // GET SESSION BY ID
     app.get('/sessions/:id', async (req, res) => {
         try {
-            const session = await service.get('sessions', req.params.id);
+            console.log("Fetching session with id: ", req.params.id)
+            const session = await service.get('sessions', { _id: new ObjectId(req.params.id) });
             if (session === null) {
                 res.status(404).json({ error: 'Session not found' });
             } else {
@@ -100,10 +66,13 @@ function gympalAPI(app) {
         }
     });
 
+    // CREATE SESSION
     app.post('/sessions', async (req, res) => {
         try {
             const session = req.body;
-            await service.create('sessions', session);
+            // Extract the fields from the data object, excluding _id
+            const { _id, ...updateData } = data;
+            await service.create('sessions', updateData);
             res.status(201).json({success: true, session});
         } catch (error) {
             console.error(error);
@@ -111,10 +80,13 @@ function gympalAPI(app) {
         }
     });
 
+    // UPDATE SESSION
     app.put('/sessions/:id', async (req, res) => {
         try {
             const session = req.body;
-            await service.update('sessions', req.params.id, session);
+            // Extract the fields from the data object, excluding _id
+            const { _id, ...updateData } = session;
+            await service.update('sessions', req.params.id, updateData);
             res.status(200).json({success: true, session});
         } catch (error) {
             console.error(error);
--- services/service.js
+++ services/service.js
@@ -1,79 +1,296 @@
 const MongoLib = require('../lib/mongo');
+const { ObjectId } = require('mongodb');
 
 class Service {
     constructor() {
         this.mongoDB = new MongoLib();
-        //this.populateDB();
+        // this.dropEvertything().then(() => {
+        //     this.populateDB();
+        //     console.log("DB DROPPED AND POPULATED")
+        // });
+    }
 
-        
+    async dropEvertything(){
+        this.mongoDB.dropCollection('exercises');
+        this.mongoDB.dropCollection('exercise_stats');
+        this.mongoDB.dropCollection('sessions');
     }
 
     async populateDB(){
 
-        //Delete all collections
-        this.mongoDB.dropCollection('exercises');
-        this.mongoDB.dropCollection('exercise_stats');
-        this.mongoDB.dropCollection('sessions');
+        const NUM_SESSIONS = 5;
+        const EXERCISES_PER_SESSION = 5;
+        const STATS_ENTRIES = 5;
 
         //ADD MOCK DATA
         let exercises = [ "Press de banca", "Sentadillas", "Peso muerto", "Dominadas", "Fondos en paralelas", "Curl de bíceps con barra", "Curl de bíceps con mancuernas", "Prensa de hombros", "Remo con barra", "Remo con mancuernas", "Elevaciones laterales", "Patada de tríceps", "Flexiones de pecho", "Elevaciones de gemelos", "Plancha", "Abdominales", "Sentadillas búlgaras", "Peso muerto rumano", "Zancadas", "Hip Thrust", "Burpees", "Mountain climbers", "Flexiones diamante", "Flexiones de tríceps", "Flexiones inclinadas" ];
         console.log("Adding " + exercises.length + " exercises");
-        exercises.forEach(exercise => {
-            this.mongoDB.create('exercises', {name: exercise});
-        });
-        exercises = await this.mongoDB.getAll('exercises');
 
-        //Add exerciseStats
-        exercises.forEach(exercise => {
-            this.mongoDB.create('exercise_stats', {
-                exerciseId: exercise._id,
-                repeticionMaxima: Math.floor(Math.random() * 100) + 1,
-                pesoEfectivo: Math.floor(Math.random() * 100) + 1,
-                volumenTotal: Math.floor(Math.random() * 100) + 1,
-            });
-        });
+        //EXERCISES 
+        exercises.forEach(async exercise => {
 
-
-        //Add Sessions
-        const numSessions = 5;
-        console.log("Adding " + numSessions + " sessions");
-        for (let i = 0; i < numSessions; i++) {
-            //exerciseLogs
-            let exerciseLogs = [];
-
-            for (let j = 0; j < 5; j++) {
-                //Save a random exercise name and id
-                const exercise = exercises[Math.floor(Math.random() * exercises.length)];
-                exerciseLogs.push({
-                    exerciseId: exercise._id,
-                    name: exercise.name,
-                    kgs: Math.floor(Math.random() * 100) + 1,
-                    reps: Math.floor(Math.random() * 20) + 1,
-                    sets: Math.floor(Math.random() * 10) + 1,
-                });
+            // New object
+            let newExercise = {
+                name: exercise,
+                repeticionMaxima: [],
+                pesoEfectivo: [],
+                volumenTotal: []
             }
 
-            this.mongoDB.create('sessions', {
-                name: `Session ${i+1}`,
-                date: new Date(),
-                stats: {
-                    puntos: Math.floor(Math.random() * 100) + 1,
-                    kgsMovidos: Math.floor(Math.random() * 1000) + 1,
-                    kcalsQuemadas: Math.floor(Math.random() * 1000) + 1,
-                },
-                exercises: exerciseLogs,
-            });
-        }
-        console.log("FINISHED POPULATING DB")
+            //Populate stats
+            for (let i = 0; i < STATS_ENTRIES; i++) {
+                let date = new Date();
+                date.setDate(date.getDate() - i);
+                date = date.toLocaleDateString('es-ES');
+                newExercise.repeticionMaxima.push({ date: date, value: Math.floor(Math.random() * 100) + 1 });
+                newExercise.pesoEfectivo.push({ date: date, value: Math.floor(Math.random() * 100) + 1 });
+                newExercise.volumenTotal.push({ date: date, value: Math.floor(Math.random() * 100) + 1 });
+            }
+
+            //Add exercise
+            await this.mongoDB.create('exercises', newExercise);
+        });
+
+        //sleep for 1 second
+        await new Promise(resolve => setTimeout(resolve, 1000));
+
+
+        //SESSIONS
+        let exercisesData = await this.mongoDB.getAll('exercises').then(async (exercisesData) => {
+            for (let i = 0; i < NUM_SESSIONS; i++) {
+                let exerciseLogs = [];
+                for (let j = 0; j < EXERCISES_PER_SESSION; j++) {
+                    const exercise = exercisesData[Math.floor(Math.random() * exercisesData.length)];
+                    exerciseLogs.push({
+                        exerciseId: exercise._id,
+                        name: exercise.name,
+                        kgs: Math.floor(Math.random() * 100) + 1,
+                        reps: Math.floor(Math.random() * 20) + 1,
+                        sets: Math.floor(Math.random() * 10) + 1,
+                    });
+                }
+
+                let date = new Date();
+                date.setDate(date.getDate() - i);
+                date = date.toLocaleDateString('es-ES');
+
+                this.mongoDB.create('sessions', {
+                    name: `Session ${i+1}`,
+                    date: date,
+                    stats: {
+                        puntos: Math.floor(Math.random() * 100) + 1,
+                        kgsMovidos: Math.floor(Math.random() * 1000) + 1,
+                        kcalsQuemadas: Math.floor(Math.random() * 1000) + 1,
+                    },
+                    exercises: exerciseLogs,
+                });
+            }
+        });
     }
+
+
+
+
+        // await Promise.all(exercises.map(async exercise => {
+        //     //Add exercise
+        //     await this.mongoDB.create('exercises', {name: exercise});
+        //     //Add exercise stats for that exercise
+        //     let exerciseData = await this.mongoDB.find('exercises', {name: exercise});
+
+        //     console.log("Added exercise: " + exerciseData.name + " with id: " + exerciseData._id)
+            
+        //     let repeticionMaxima = [];
+        //     let pesoEfectivo = [];
+        //     let volumenTotal = [];
+
+        //     for (let i = 0; i < STATS_ENTRIES; i++) {
+        //         let date = new Date();
+        //         date.setDate(date.getDate() - i);
+        //         date = date.toLocaleDateString('es-ES');
+        //         repeticionMaxima.push({ date: date, value: Math.floor(Math.random() * 100) + 1 });
+        //         pesoEfectivo.push({ date: date, value: Math.floor(Math.random() * 100) + 1 });
+        //         volumenTotal.push({ date: date, value: Math.floor(Math.random() * 100) + 1 });
+        //     }
+
+        //     console.log("    Adding stats for exercise: " + exerciseData.name + " with id: " + exerciseData._id);
+            
+        // }
+        // )).then(async () => {
+        //     //SESSIONS
+        //     let exercisesData = await this.mongoDB.getAll('exercises');
+        //     for (let i = 0; i < NUM_SESSIONS; i++) {
+        //         let exerciseLogs = [];
+        //         for (let j = 0; j < EXERCISES_PER_SESSION; j++) {
+        //             const exercise = exercisesData[Math.floor(Math.random() * exercisesData.length)];
+        //             exerciseLogs.push({
+        //                 exerciseId: exercise._id,
+        //                 name: exercise.name,
+        //                 kgs: Math.floor(Math.random() * 100) + 1,
+        //                 reps: Math.floor(Math.random() * 20) + 1,
+        //                 sets: Math.floor(Math.random() * 10) + 1,
+        //             });
+        //         }
+
+        //         let date = new Date();
+        //         date.setDate(date.getDate() - i);
+        //         date = date.toLocaleDateString('es-ES');
+
+        //         this.mongoDB.create('sessions', {
+        //             name: `Session ${i+1}`,
+        //             date: date,
+        //             stats: {
+        //                 puntos: Math.floor(Math.random() * 100) + 1,
+        //                 kgsMovidos: Math.floor(Math.random() * 1000) + 1,
+        //                 kcalsQuemadas: Math.floor(Math.random() * 1000) + 1,
+        //             },
+        //             exercises: exerciseLogs,
+        //         });
+        //     }
+        // });
+
+        // await Promise.all(exercises.map(async exercise => {
+        //     //Add exercise
+        //     await this.mongoDB.create('exercises', {name: exercise});
+            
+        // })).then(async () => {
+        //     let exercisesData = await this.mongoDB.getAll('exercises');
+
+        //     //EXERCISE_STATS
+        //     await Promise.all(exercisesData.map(exercise => {
+        //         let repeticionMaxima = [];
+        //         let pesoEfectivo = [];
+        //         let volumenTotal = [];
+
+        //         for (let i = 0; i < STATS_ENTRIES; i++) {
+        //             let date = new Date();
+        //             date.setDate(date.getDate() - i);
+        //             date = date.toLocaleDateString('es-ES');
+        //             repeticionMaxima.push({ date: date, value: Math.floor(Math.random() * 100) + 1 });
+        //             pesoEfectivo.push({ date: date, value: Math.floor(Math.random() * 100) + 1 });
+        //             volumenTotal.push({ date: date, value: Math.floor(Math.random() * 100) + 1 });
+        //         }
+
+        //         console.log("Adding stats for exercise: " + exercise.name + " with id: " + exercise._id);
+        //         this.mongoDB.create('exercise_stats', {
+        //             exerciseId: exercise._id,
+        //             exerciseName: exercise.name,
+        //             repeticionMaxima: repeticionMaxima,
+        //             pesoEfectivo: pesoEfectivo,
+        //             volumenTotal: volumenTotal,
+        //         });
+        //     })).then(async () => {
+
+        //         //SESSIONS
+        //         let exercisesData = await this.mongoDB.getAll('exercises');
+        //         for (let i = 0; i < NUM_SESSIONS; i++) {
+        //             let exerciseLogs = [];
+        //             for (let j = 0; j < EXERCISES_PER_SESSION; j++) {
+        //                 const exercise = exercisesData[Math.floor(Math.random() * exercisesData.length)];
+        //                 exerciseLogs.push({
+        //                     exerciseId: exercise._id,
+        //                     name: exercise.name,
+        //                     kgs: Math.floor(Math.random() * 100) + 1,
+        //                     reps: Math.floor(Math.random() * 20) + 1,
+        //                     sets: Math.floor(Math.random() * 10) + 1,
+        //                 });
+        //             }
+
+        //             let date = new Date();
+        //             date.setDate(date.getDate() - i);
+        //             date = date.toLocaleDateString('es-ES');
+
+        //             this.mongoDB.create('sessions', {
+        //                 name: `Session ${i+1}`,
+        //                 date: date,
+        //                 stats: {
+        //                     puntos: Math.floor(Math.random() * 100) + 1,
+        //                     kgsMovidos: Math.floor(Math.random() * 1000) + 1,
+        //                     kcalsQuemadas: Math.floor(Math.random() * 1000) + 1,
+        //                 },
+        //                 exercises: exerciseLogs,
+        //             });
+        //         }
+        //     });
+        // });
+        
+        
+
+
+
+
+
+
+        // exercises.forEach(exercise => {
+        //     //EXERCISES
+        //     this.mongoDB.create('exercises', {name: exercise});
+        // }).then(() => {
+        //     //EXERCISE_STATS
+        //     this.mongoDB.getAll('exercises').then((exercises) => {
+        //         exercises.forEach(exercise => {
+        //             let repeticionMaxima = [];
+        //             let pesoEfectivo = [];
+        //             let volumenTotal = [];
+
+        //             for (let i = 0; i < STATS_ENTRIES; i++) {
+        //                 let date = new Date();
+        //                 date.setDate(date.getDate() - i);
+        //                 date = date.toLocaleDateString('es-ES');
+        //                 repeticionMaxima.push({ date: date, value: Math.floor(Math.random() * 100) + 1 });
+        //                 pesoEfectivo.push({ date: date, value: Math.floor(Math.random() * 100) + 1 });
+        //                 volumenTotal.push({ date: date, value: Math.floor(Math.random() * 100) + 1 });
+        //             }
+
+        //             this.mongoDB.create('exercise_stats', {
+        //                 exerciseId: exercise._id,
+        //                 exerciseName: exercise.name,
+        //                 repeticionMaxima: repeticionMaxima,
+        //                 pesoEfectivo: pesoEfectivo,
+        //                 volumenTotal: volumenTotal,
+        //             });
+        //         });
+        //     });
+        // }).then(() => {
+        //     //SESSIONS
+        //     this.mongoDB.getAll('exercises').then((exercises) => {  
+        //         for (let i; i < NUM_SESSIONS; i++) {
+        //             let exerciseLogs = [];
+        //             for (let j = 0; j < EXERCISES_PER_SESSION; j++) {
+        //                 const exercise = exercises[Math.floor(Math.random() * exercises.length)];
+        //                 exerciseLogs.push({
+        //                     exerciseId: exercise._id,
+        //                     name: exercise.name,
+        //                     kgs: Math.floor(Math.random() * 100) + 1,
+        //                     reps: Math.floor(Math.random() * 20) + 1,
+        //                     sets: Math.floor(Math.random() * 10) + 1,
+        //                 });
+        //             }
+
+        //             let date = new Date();
+        //             date.setDate(date.getDate() - i);
+        //             date = date.toLocaleDateString('es-ES');
+
+        //             this.mongoDB.create('sessions', {
+        //                 name: `Session ${i+1}`,
+        //                 date: date,
+        //                 stats: {
+        //                     puntos: Math.floor(Math.random() * 100) + 1,
+        //                     kgsMovidos: Math.floor(Math.random() * 1000) + 1,
+        //                     kcalsQuemadas: Math.floor(Math.random() * 1000) + 1,
+        //                 },
+        //                 exercises: exerciseLogs,
+        //             });
+        //         }
+        //     });
+        // });
+
     
 
     async getAll(collection) {
         return this.mongoDB.getAll(collection);
     }
 
-    async get(collection, id) {
-        return this.mongoDB.get(collection, id);
+    async get(collection, query) {
+        return this.mongoDB.get(collection, query);
     }
 
     async create(collection, data) {
